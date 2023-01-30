0de4f2a0c40dd57f823c1abea09f5a16055b5c03
Paige Melcher <pmelcher@uvm.edu>
2023-01-30 14:21:28 -0500

    created a hook robot

--- generate.py
+++ generate.py
@@ -8,8 +8,14 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5] , size=[1,1,1])
-    pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0.5,0,1.0])
-    pyrosim.Send_Cube(name="Link1", pos=[1,0,1.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1.0])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1.0])
+    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint(name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0] , size=[1,1,1])
+    #pyrosim.Send_Joint(name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,0.5,0.5])
+    #pyrosim.Send_Cube(name="Link4", pos=[0,0.5,0] , size=[1,1,1])
     pyrosim.End()
 
 if __name__ == "__main__":
