8cf9e36997df92bda9fc9c169272df67fd605b47
Paige Melcher <pmelcher@uvm.edu>
2023-01-29 23:22:42 -0500

    created a world link and a robot link

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[2,3,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.End()
+
+if __name__ == "__main__":
+    Create_World()
+    Create_Robot()
+
