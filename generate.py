e4b9d11a1f44248cc4c06dd1fdabd3a03f35eb31
Paige Melcher <pmelcher@uvm.edu>
2023-01-29 22:52:06 -0500

    created a new, minimal world

--- generate.py
+++ generate.py
@@ -1,15 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = 0.5
-x2 = 0
-y2 = 1
-z2 = 1.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
 pyrosim.End()
