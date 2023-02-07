50aeb8bdee6fe453859a3b798a8e109eecfe6e57
Paige Melcher <pmelcher@uvm.edu>
2023-02-07 10:43:37 -0500

    changed statement to decode using UTF-8 in pyrosim to prevent keyError

--- simulate.py
+++ simulate.py
@@ -20,7 +20,7 @@ for i in range(0,1000):
     time.sleep(1/60)
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = ..., maxForce = ...)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
 
 print(backLegSensorValues)
 print(frontLegSensorValues)
