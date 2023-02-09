6cecdebb4cf1b00aeead9008617e3e25fb44a66d
Paige Melcher <pmelcher@uvm.edu>
2023-02-08 19:52:11 -0500

    created targetAngles for values that vary sinusoidal

--- simulate.py
+++ simulate.py
@@ -19,6 +19,9 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
+radianValues = np.linspace(0, 2*np.pi, num= 1000)
+targetAngles = (np.pi/4)*np.sin(radianValues)
+
 for i in range(0,1000):
     p.stepSimulation()
     time.sleep(1/60)
@@ -28,13 +31,13 @@ for i in range(0,1000):
                                 jointName = "Torso_BackLeg", 
                                 controlMode = p.POSITION_CONTROL, 
                                 targetPosition = random.uniform(-PI/2, PI/2), 
-                                maxForce = 50)
+                                maxForce = 40)
     
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, 
                                 jointName = "Torso_FrontLeg", 
                                 controlMode = p.POSITION_CONTROL, 
                                 targetPosition = random.uniform(-PI/2, PI/2), 
-                                maxForce = 50)
+                                maxForce = 40)
 
 print(backLegSensorValues)
 print(frontLegSensorValues)
