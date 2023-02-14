bcc35a708ffdfda0128a907eebd3abff02f9cd53
Paige Melcher <pmelcher@uvm.edu>
2023-02-13 23:25:51 -0500

    robot moving away from origin

--- simulate.py
+++ simulate.py
@@ -7,9 +7,12 @@ import math
 import random
 
 PI = math.pi
-amplitude = PI/4
-frequency = 5
-phaseOffset = 0
+frontAmplitude = PI/3
+backAmplitude = PI/4
+frontFrequency = 8
+backFrequency = 10
+frontPhaseOffset = 0
+backPhaseOffset = 0
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -28,12 +31,12 @@ targetAngles = (np.pi/4)*np.sin(radianValues)
 backLegTargetAngles = np.zeros(1000)
 backLegRadianValues = np.linspace(0, 2*np.pi, num= 1000)
 for i in range(1000):
-    backLegTargetAngles[i] = amplitude*np.sin(frequency*backLegRadianValues[i] + phaseOffset)
+    backLegTargetAngles[i] = backAmplitude*np.sin(backFrequency*backLegRadianValues[i] + backPhaseOffset)
 
 frontLegTargetAngles = np.zeros(1000)
 frontLegRadianValues = np.linspace(0, 2*np.pi, num= 1000)
 for i in range(1000):
-    frontLegTargetAngles[i] = amplitude*np.sin(frequency*frontLegRadianValues[i] + phaseOffset)
+    frontLegTargetAngles[i] = frontAmplitude*np.sin(frontFrequency*frontLegRadianValues[i] + frontPhaseOffset)
 
 #np.save("data/backTargetAngleValues.npy", backLegTargetAngles)
 #np.save("data/frontTargetAngleValues.npy", frontLegTargetAngles)
@@ -56,11 +59,11 @@ for i in range(0,1000):
                                 targetPosition = frontLegTargetAngles[i], 
                                 maxForce = 40)
 
-print(backLegSensorValues)
-print(frontLegSensorValues)
+#print(backLegSensorValues)
+#print(frontLegSensorValues)
 
-np.save("data/backLegSensorValues.npy", backLegSensorValues)
-np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
+#np.save("data/backLegSensorValues.npy", backLegSensorValues)
+#np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 
 
 p.disconnect()
