fde3d1677ef8ff6eb64ea5dabbb15f22872f10a2
Paige Melcher <pmelcher@uvm.edu>
2023-02-14 11:01:03 -0500

    added the classes' constructors

--- robot.py
+++ robot.py
@@ -1,4 +1,8 @@
+import pybullet as p
+import pybullet_data
+
 class ROBOT:
     def __init__(self):
         self.sensors = {}
         self.motors = {}
+        self.robotId = p.loadURDF("body.urdf")
--- simulation.py
+++ simulation.py
@@ -1,4 +1,12 @@
+import pybullet as p
+import pybullet_data
+from world import WORLD
+from robot import ROBOT
+
 class SIMULATION:
     def __init__(self):
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8)
         self.world = WORLD()
         self.robot = ROBOT()
