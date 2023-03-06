43d634af8eb0b4ea1ed1a8fee667bea188f649ec
Paige Melcher <pmelcher@uvm.edu>
2023-03-06 18:13:18 -0500

    started creating new methods in solution and hillclimber

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import os
 import hillclimber
 
 hc = hillclimber.HILL_CLIMBER()
+hc.Evolve()
 
 #for i in range(5):
     #os.system("python3 generate.py")
--- solution.py
+++ solution.py
@@ -4,3 +4,6 @@ class SOLUTION:
     def __init__(self):
         self.weights = np.random.rand(3,2)
         result = self.weights * 2 - 1
+    
+    def Evaluate(self):
+        pass
