0cd32e959dd26b0361e68318afcc4b10f04297b5
Paige Melcher <pmelcher@uvm.edu>
2023-03-09 18:05:56 -0500

    modifying evaluate function

--- hillclimber.py
+++ hillclimber.py
@@ -3,6 +3,6 @@ from solution import SOLUTION
 class HILL_CLIMBER:
     def __init__(self):
         self.parent = SOLUTION()
-    #Create a new method for SOLUTION called Evaluate(). Call this from self.parent in HILL_CLIMBER's Evolve() method.
-    def Evolve():
+
+    def Evolve(self):
         self.parent.Evaluate()
