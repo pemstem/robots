8f04e2b1403c99a31e76bea61a540779b7d82d0c
Paige Melcher <pmelcher@uvm.edu>
2023-03-09 18:42:42 -0500

    select function

--- hillclimber.py
+++ hillclimber.py
@@ -24,6 +24,7 @@ class HILL_CLIMBER:
         self.child.Mutate()
 
     def Select(self):
-        pass
+        if self.child.fitness > self.parent.fitness:
+            self.parent = self.child
 
 
